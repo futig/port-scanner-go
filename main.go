6eca440bea04d336e4dfca6998366b2c3cd50335
futig <everymusic8@gmail.com>
2024-11-17 01:24:54 +0500

    main

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package PortScannerGo
+package main
+
+import (
+	"fmt"
+	"os"
+
+	cli "github.com/futig/PortScannerGo/presentation"
+)
+
+func main() {
+	cfg, err := cli.ParseArgs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v", cfg)
+}
